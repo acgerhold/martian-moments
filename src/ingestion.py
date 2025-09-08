1cfd1a712930f5d7487f4ed2851adf09ebf22630
acg <acgerhold@gmail.com>
2025-09-08 13:45:09 -0500

    More DAG cleanup, moved .json file creation logic out of dags and to ingestion.py. Created unit tests for these two new functions.

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,6 +1,8 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime, timezone
+
 
 load_dotenv()
 NASA_KEY = os.getenv('NASA_KEY')
@@ -19,7 +21,6 @@ def extract_photos_from_nasa(rover: str, sol: int, logger):
         return photos_response
     except Exception as e:
         error_msg = f"Error processing photos request for rover: {rover} on sol: {sol}: {e}"
-        print(error_msg)
         logger.error(error_msg)
         return {"photos": []}
 
@@ -38,6 +39,51 @@ def extract_manifest_from_nasa(rover: str, logger):
         return manifest_response
     except Exception as e:
         error_msg = f"Error processing manifest request for rover {rover}: {e}"
-        print(error_msg)
         logger.error(error_msg)
         return {"manifest": []}
+    
+def create_final_json(rover, sol, photos_result):
+    if photos_result:
+        photo_count = len(photos_result.get('photos', []))
+        photos = photos_result.get('photos', [])
+        ingestion_timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S")
+        filename = f"{rover.lower()}_photos_sol_{sol}_{ingestion_timestamp}.json"
+        
+        final_json = {
+            "filename": filename,
+            "sol_start": sol,
+            "sol_end": sol,
+            "photo_count": photo_count,
+            "photos": photos,
+            "ingestion_date": ingestion_timestamp,
+        }
+
+        return final_json
+    
+def create_final_batch_json(sols, all_rover_photos_results):
+        sol_start = min(sols)
+        sol_end = max(sols)
+
+        all_photos = []
+        for result in all_rover_photos_results:
+            for photo_array in result:
+                if photo_array:
+                    all_photos.extend(photo_array)
+
+        photo_count = len(all_photos)        
+        ingestion_timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S")
+        if sol_start == sol_end:
+            filename = f"mars_rover_photos_batch_sol_{sol_start}_{ingestion_timestamp}.json"
+        else:
+            filename = f"mars_rover_photos_batch_sol_{sol_start}_to_{sol_end}_{ingestion_timestamp}.json"
+                
+        final_json = {
+            "filename": filename,
+            "sol_start": sol_start,
+            "sol_end": sol_end,
+            "photo_count": photo_count,
+            "photos": all_photos,
+            "ingestion_date": ingestion_timestamp
+        }
+
+        return final_json
