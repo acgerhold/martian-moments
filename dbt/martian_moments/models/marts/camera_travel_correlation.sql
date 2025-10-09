2a0859b152581eddec534ec8faa7822ee2c78ab3
acg <acgerhold@gmail.com>
2025-10-09 17:14:14 -0500

    Optimized camera_travel_correlation mart. All models have proper incremental filtering now. Few minor optimization tweaks still to bemade like camera_id. Snowflake credit consumption has gone down significantly over time.

--- dbt/martian_moments/models/marts/camera_travel_correlation.sql
+++ dbt/martian_moments/models/marts/camera_travel_correlation.sql
@@ -1,8 +1,8 @@
 {{ config(
     materialized='incremental',
-    unique_key=['name', 'sol', 'camera_full_name', 'travel_time_start', 'image'],
+    unique_key=['image_link'],
     incremental_strategy='append',
-    cluster_by=['name', 'sol', 'camera_full_name', 'travel_time_start', 'image'],
+    cluster_by=['rover_name', 'sol_number', 'camera_name'],
     tags='aggregate'
 ) }}
 
@@ -10,31 +10,33 @@ WITH photo_with_time AS (
     SELECT
         fph.rover_id,
         fph.sol,
-        fph.camera_id,
+        fph.camera_name,
         fph.img_src,
-        REGEXP_SUBSTR(fph.img_src, '_([0-9]{10})_', 1, 1, 'e', 1)::BIGINT AS photo_time
+        REGEXP_SUBSTR(fph.img_src, '_([0-9]{10})_', 1, 1, 'e', 1)::BIGINT AS photo_time,
+        fph.ingestion_date
     FROM 
         {{ source('MARS_SILVER', 'FACT_PHOTOS') }} fph
     WHERE 
         fph.rover_id = 8
         {% if is_incremental() %}
-            AND fph.ingestion_date = (SELECT MAX(ingestion_date) FROM {{ source('MARS_SILVER', 'FACT_PHOTOS') }})
+            AND fph.ingestion_date > (SELECT MAX(ingestion_date) FROM {{ this }})
         {% endif %}
 )
 SELECT
-    dro.rover_name AS name,
-    pwt.sol,
-    dca.camera_full_name,
+    dro.rover_name AS rover_name,
+    pwt.sol AS sol_number,
+    dca.camera_name AS camera_name,
     fpa.sclk_start AS travel_time_start,
     fpa.sclk_end AS travel_time_end,
-    pwt.img_src AS image,
-    pwt.photo_time,
-    pwt.photo_time BETWEEN fpa.sclk_start AND fpa.sclk_end AS taken_during_travel
+    pwt.img_src AS image_link,
+    pwt.photo_time AS photo_time,
+    pwt.photo_time BETWEEN fpa.sclk_start AND fpa.sclk_end AS taken_during_travel,
+    pwt.ingestion_date AS ingestion_date
 FROM 
     photo_with_time pwt
 JOIN 
     {{ source('MARS_SILVER', 'DIM_ROVERS') }} dro ON pwt.rover_id = dro.rover_id
 JOIN 
-    {{ source('MARS_SILVER', 'DIM_CAMERAS') }} dca ON pwt.camera_id = dca.camera_id
+    {{ source('MARS_SILVER', 'DIM_CAMERAS') }} dca ON pwt.rover_id = dca.rover_id AND pwt.camera_name = dca.camera_name
 INNER JOIN 
     {{ source('MARS_SILVER', 'FACT_PATH') }} fpa ON pwt.rover_id = fpa.rover_id AND pwt.sol = fpa.sol
